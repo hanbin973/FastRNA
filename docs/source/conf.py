71d76e75f8bc03542f7db50ef002bda66e1c2813
hanbin973 <hanbin973>
2022-08-16 14:10:32 +0900

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'FastRNA'
-copyright = '2022, Hanbin Lee'
-author = 'Hanbin Lee'
-release = '0.1.0'
+project = "FastRNA"
+copyright = "2022, Hanbin Lee"
+author = "Hanbin Lee"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "nbsphinx",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
