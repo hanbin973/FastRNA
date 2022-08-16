663bc8a212ad0ba2c33919fa73fe947751481544
hanbin973 <hanbin973>
2022-08-16 15:30:29 +0900

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.patch.abspath('../../fastrna/'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -16,6 +20,7 @@ release = "0.1.0"
 
 extensions = [
     "sphinx_rtd_theme",
+	"sphinx.ext.autodoc",
     "nbsphinx",
 ]
 
